f708eeb136edcc8d3a422098ea7007d0c7b8b682
vamshi-907 <148422443+vamshi-907@users.noreply.github.com>
2026-01-08 00:36:35 +0530

    Change API base URL to production endpoint

--- frontend/lib/api.js
+++ frontend/lib/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://fullstack-intern-assignment-z2rp.onrender.com/api",
 });
 
 export const setToken = (token) => {
